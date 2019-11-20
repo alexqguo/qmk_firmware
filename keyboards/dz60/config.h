ffcada9c4ef8106da3d0ce713f9e31e4e6256998
Alex Guo <alexguo@amazon.com>
2019-11-20 13:37:31 -0500

    Add custom layout for dz60 ansi split

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -11,6 +11,19 @@
 #define PRODUCT         DZ60
 #define DESCRIPTION     DZ60 Keyboard
 
+/* ALEXGUO mouse configuration */
+// acl0 base speed, acl1 slow speed, acl2 fast speed
+#define MOUSEKEY_DELAY 0 // Reduce delay of mouse key press to movement
+#define MOUSEKEY_INTERVAL 16 // for smooth mouse movement
+#define MOUSEKEY_TIME_TO_MAX 0 // disabling acceleration
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0 // disabling acceleration for scrolling
+#define MK_3_SPEED // define mk3 speed to enable constant mode
+#define MK_C_INTERVAL_0 16 // smooth movement for base speed (other layers default to this)
+#define MK_C_OFFSET_0 10 // base mouse speed
+#define MK_C_OFFSET_1 3 // acl1 speed (slow)
+#define MK_C_OFFSET_2 40 // acl2 speed (fast)
+#define MK_MOMENTARY_ACCEL // enable momentary acceleration mode
+
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
